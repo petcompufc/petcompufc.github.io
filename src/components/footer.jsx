17d12e88fa7a0e735a081cffc342e051d75790c9
Felipe Keiler <fkeiler@alu.ufc.br>
2021-03-03 11:30:19 -0300

    ⚡️ Otimização das imagens do footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 import {
+  useTheme,
   Box,
-  Image,
   List,
   ListItem,
   Link,
@@ -10,92 +11,99 @@ import {
   Grid,
 } from '@chakra-ui/react';
 
-import { RiFacebookCircleLine, RiGithubLine, RiInstagramLine, RiMailSendLine, RiYoutubeLine } from 'react-icons/ri';
-
-import PETLogo from '../images/logo_pet-white.png';
-import DCLogo from '../images/logo_dc.png';
-import UFCLogo from '../images/logo_ufc.png';
+import {
+  RiFacebookCircleLine,
+  RiGithubLine,
+  RiInstagramLine,
+  RiMailSendLine,
+  RiYoutubeLine,
+} from 'react-icons/ri';
 
 export default function Footer() {
+  const theme = useTheme();
+
   return (
-    <Box
+    <Grid
       as="footer"
       bg="#37474f"
-      overflow="hidden"
-      maxWidth="960"
-      padding="1.45rem 1.0875rem"
-      color="#fff"
-      width="100%"
+      color="#ffffff"
+      px={`calc(50% - calc(${theme.sizes.container.xl} / 2))`}
+      py={5}
+      w="100%"
+      fontSize="16px"
+      templateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(4, 1fr)']}
     >
-      <Grid
-        maxW="1280px"
-        w="100%"
+
+      <Box
+        maxW="300px"
         mx="auto"
-        fontSize="16px"
-        templateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(4, 1fr)']}
+        float="left"
+        boxSizing="border-box"
       >
+        <StaticImage
+          alt="PETComp UFC"
+          src="../images/logo-pet-white.svg"
+          width={300}
+        />
+      </Box>
 
-        <Box
-          mx="auto"
-          float="left"
-          boxSizing="border-box"
-        >
-          <Image
-            w="90%"
-            maxW="300px"
-            src={PETLogo}
-            alt="Logo do pet branca."
+      <List
+        mx="auto"
+        float="left"
+        minH="1px"
+        spacing="1"
+        fontWeight="lighter"
+      >
+        <ListItem fontSize="xl" fontWeight="bold">PET Computação</ListItem>
+        <ListItem my={0}>Av. Humberto Monte, s/n</ListItem>
+        <ListItem my={0}>UFC - Campus do Pici</ListItem>
+        <ListItem my={0}>Departamento da Computação</ListItem>
+        <ListItem my={0}>Bloco 910</ListItem>
+        <ListItem my={0}>petcomp@ufc.br</ListItem>
+        <Grid templateColumns="repeat(3, 1fr)" rowGap={3} columnGap={2} fontSize="xl">
+          <Link href="https://github.com/petcompufc" mx="auto" isExternal color="white" textDecoration="initial"><RiGithubLine /></Link>
+          <Link href="https://www.facebook.com/PETCompUFC/" mx="auto" isExternal color="white"><RiFacebookCircleLine /></Link>
+          <Link href="https://www.instagram.com/petcompufc" mx="auto" isExternal color="white"><RiInstagramLine /></Link>
+          <Link href="https://www.youtube.com/user/petcompufc/videos" mx="auto" isExternal color="white"><RiYoutubeLine /></Link>
+          <Link href="mailto:petcomp@ufc.br" mx="auto" isExternal color="white"><RiMailSendLine /></Link>
+        </Grid>
+      </List>
+
+      <List
+        mx="auto"
+        float="left"
+        minH="1px"
+        spacing="1"
+        fontWeight="lighter"
+      >
+        <ListItem fontSize="24px" fontWeight="bold">Links</ListItem>
+        <ListItem>
+          <Link href="http://www.ufc.br/" isExternal color="white">Portal UFC</Link>
+        </ListItem>
+        <ListItem marginTop="-10px">
+          <Link href="http://portal.dc.ufc.br/" isExternal color="white">Departamento de Computação</Link>
+        </ListItem>
+        <ListItem marginTop="-10px">
+          <Link href="https://cc.ufc.br/" isExternal color="white">Curso de Ciência da Computação</Link>
+        </ListItem>
+      </List>
+
+      <Stack
+        mx="auto"
+      >
+        <Link mx="auto" w="90%" href="http://www.ufc.br/" isExternal>
+          <StaticImage
+            alt="Universidade Federal do Ceará"
+            src="../images/logo-ufc.svg"
           />
-        </Box>
-
-        <List
-          mx="auto"
-          float="left"
-          minH="1px"
-          spacing="1"
-          fontWeight="lighter"
-        >
-          <ListItem fontSize="xl" fontWeight="bold">PET Computação</ListItem>
-          <ListItem my={0}>Av. Humberto Monte, s/n</ListItem>
-          <ListItem my={0}>UFC - Campus do Pici</ListItem>
-          <ListItem my={0}>Departamento da Computação</ListItem>
-          <ListItem my={0}>Bloco 910</ListItem>
-          <ListItem my={0}>petcomp@ufc.br</ListItem>
-          <Grid templateColumns="repeat(3, 1fr)" rowGap={3} columnGap={2} fontSize="xl">
-            <Link href="https://github.com/petcompufc" mx="auto" isExternal color="white" textDecoration="initial"><RiGithubLine /></Link>
-            <Link href="https://www.facebook.com/PETCompUFC/" mx="auto" isExternal color="white"><RiFacebookCircleLine /></Link>
-            <Link href="https://www.instagram.com/petcompufc" mx="auto" isExternal color="white"><RiInstagramLine /></Link>
-            <Link href="https://www.youtube.com/user/petcompufc/videos" mx="auto" isExternal color="white"><RiYoutubeLine /></Link>
-            <Link href="mailto:petcomp@ufc.br" mx="auto" isExternal color="white"><RiMailSendLine /></Link>
-          </Grid>
-        </List>
-
-        <List
-          mx="auto"
-          float="left"
-          minH="1px"
-          spacing="1"
-          fontWeight="lighter"
-        >
-          <ListItem fontSize="24px" fontWeight="bold">Links</ListItem>
-          <ListItem>
-            <Link href="http://www.ufc.br/" isExternal color="white">Portal UFC</Link>
-          </ListItem>
-          <ListItem marginTop="-10px">
-            <Link href="http://portal.dc.ufc.br/" isExternal color="white">Departamento de Computação</Link>
-          </ListItem>
-          <ListItem marginTop="-10px">
-            <Link href="https://cc.ufc.br/" isExternal color="white">Curso de Ciência da Computação</Link>
-          </ListItem>
-        </List>
-
-        <Stack
-          mx="auto"
-        >
-          <Link mx="auto" w="90%" maxW="300px" href="http://www.ufc.br/" isExternal><Image src={UFCLogo} /></Link>
-          <Link mx="auto" w="85%" maxW="275px" href="http://portal.dc.ufc.br/" isExternal><Image src={DCLogo} /></Link>
-        </Stack>
-      </Grid>
-    </Box>
+        </Link>
+        <Link mx="auto" w="85%" href="http://portal.dc.ufc.br/" isExternal>
+          <StaticImage
+            alt="Departamento de Computação"
+            src="../images/logo-dc.svg"
+          />
+        </Link>
+      </Stack>
+    </Grid>
   );
 }
